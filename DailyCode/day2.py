e692ee0838309ed56da0bdb33ef4bb4345554906
anishkumar68 <anishkumar231999@gmail.com>
2026-09-02 19:55:45 +0530

    better version of day2 code

--- DailyCode/day2.py
+++ DailyCode/day2.py
@@ -1,67 +1,47 @@
-import random
+import random 
 
 
+def RockPaperScissors():
+    choices = ["Rock", "Paper", "Scissors"]
+    win_combo = [("Rock", "Scissors"), ("Paper", "Rock"), ("Scissors", "Paper")]
+    score = {"user": 0, "computer": 0, "Draw":0}
 
-class RockPaperScissors:
+    while True:
+        try:
+            choice = int(input("please choice 1 - 3 : "))
 
-    def __init__(self):
-       self.choices = None
-
-    def play(self):
-        # Display game rules
-        print("Welcome to Rock-Paper-Scissors!\n")
-        print("Winning Rules:")
-        print("Rock vs Paper -> Paper wins")
-        print("Rock vs Scissors -> Rock wins")
-        print("Paper vs Scissors -> Scissors wins\n")
-
-        self.choice = ["Rock", "Paper", "Scissors"]
-
-        while True:
-            print("chose a option:")
-            print("1. Rock")
-            print("2. Paper")
-            print("3. Scissors")
-
-            try:
-                choice = int(input("Enter your choice"))
-            except ValueError:
-                print("please enter a valid number")
+            if 1 <= choice <= 3:
+                user_choice = choices[choice - 1]
+            else:
+                print("Please choose between 1 and 3.")
                 continue
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
 
-            while choice < 1 or choice > 3 :
-                choice = int(input("please select a valid choice (1-3) : "))
+        computer_choice = random.choice(choices)
+        print(f"your choice: {user_choice}")
+        print(f"computer choice: {computer_choice}")
 
-            user_choice = self.choices[choice - 1 ]           
+        if user_choice == computer_choice:
+            score["Draw"] += 1
+            print("Draw")
+        elif (user_choice, computer_choice) in win_combo:
+            score["user"] +=1
+            print("you win!")
+        else:
+            print("you loose! computer wins!")
+            score["computer"] +=1
 
-            print("\n user choice is :", user_choice)
-            print("Now computer turn")   
+        print(f"Score: You {score['user']} - Computer {score['computer']} - Draws {score['Draw']}")
+        again = input("you want to play again.press (Y/N):").lower()
 
-            # computer choice 
-            comp_choice = random.randint(1,3)
-            computer_choice = self.choices[comp_choice - 1]
-
-            print("Computer choice is ", computer_choice)
-            print(user_choice,"vs", computer_choice)
-
-            # winner 
-            if choice == comp_choice:
-                print("Draw")  
-            elif ((choice == 1 and comp_choice == 3) or (choice == 2 and comp_choice == 1) or (choice == 3 and comp_choice)):
-                print("user win")
-            else: 
-                print("computer win")
-
-
-            while True:
-                ans = input("\n Do you want to play again ? (Y/N):").lower()
-
-                if ans == 'n':
-                    break
-                print()
-
-    print("\n Thanks for playing!")
+        if again != 'y' and again != "n":
+            print("please select a correct option Y/N:")
+    
+        if again == 'n':
+            print("Thanks for playing!")
+            break
 
 
-game = RockPaperScissors()
-game.play()
+RockPaperScissors()
